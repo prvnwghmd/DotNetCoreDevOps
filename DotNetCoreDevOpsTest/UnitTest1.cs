f4d26168b86e7743139f0d7a3f1a03966b81d13d
Pravin Waghmode <pravin.waghmode@globant.com>
2019-09-16 16:08:16 +0530

    changes in test cases

--- DotNetCoreDevOpsTest/UnitTest1.cs
+++ DotNetCoreDevOpsTest/UnitTest1.cs
@@ -1,14 +1,18 @@
+using DotNetCoreDevOps.Controllers;
 using System;
 using Xunit;
+
 
 namespace DotNetCoreDevOpsTest
 {
     public class UnitTest1
     {
+        ValuesController obj = new ValuesController();
         [Fact]
-        public void Test1()
+        public void checkById()
         {
-
+            var result = obj.Get(1);
+            Assert.Equal("value", result.Value);
         }
     }
 }
